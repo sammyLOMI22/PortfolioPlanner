f4c38bd79deaa1a06c0ec6152b577323fd068b59
sammyLOMI22 <69733709+sammyLOMI22@users.noreply.github.com>
2020-09-19 00:34:55 -0400

    v1

--- app/src/main/java/com/gothicvanitympc/portfolioplanner/MainActivity.kt
+++ app/src/main/java/com/gothicvanitympc/portfolioplanner/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gothicvanitympc.portfolioplanner
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +11,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val mybutton: Button = findViewById(R.id.calcBtn)
+        mybutton.setOnClickListener {  invCalc()}
+    }
+
+    private fun invCalc(){
+        //get investment amount
+        var invAmtS = invAmt.text.toString()
+
+        //retrive portfolio percentage
+        var lcPec = lgCapAmt.text.toString()
+        var midPec = midCapAmt.text.toString()
+        var smPec = smCapAmt.text.toString()
+        var dMPec = dMrkAmt.text.toString()
+        var eMPec = eMAmt.text.toString()
+        var sPec = sAmt.text.toString()
+        var rePec = reAmt.text.toString()
+        var dfiPec = dfiAmt.text.toString()
+        var ifcPec = ifcAmt.text.toString()
+
+        //calculate investment totals
+
+
 
     }
 }
